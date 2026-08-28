d73fae562e6210f033584f607bd53e789dfea626
Robert Caddy <rcaddy@princeton.edu>
2026-08-28 14:30:08 -0400

    fix(ode): norm was not accumulating or resetting
    `norm_new` in `NewtonSolve` was not accumulating on each inner loop
    iteration and wasn't being reset to zero between outer loop iterations.
    Fixed both issues.
    
    Signed-off-by: Robert Caddy <rcaddy@princeton.edu>

--- ode/impl/KokkosODE_Newton_impl.hpp
+++ ode/impl/KokkosODE_Newton_impl.hpp
@@ -48,7 +48,6 @@ KOKKOS_FUNCTION KokkosODE::Experimental::newton_solver_status NewtonSolve(
   const norm_type norm0 = KokkosBlas::serial_nrm2(rhs);
   norm_type norm        = Kokkos::ArithTraits<norm_type>::zero();
   norm_type norm_old    = Kokkos::ArithTraits<norm_type>::zero();
-  norm_type norm_new    = Kokkos::ArithTraits<norm_type>::zero();
   norm_type rate        = Kokkos::ArithTraits<norm_type>::zero();
 
   const norm_type tol = Kokkos::max(10 * Kokkos::ArithTraits<norm_type>::eps() / params.rel_tol,
@@ -107,8 +106,9 @@ KOKKOS_FUNCTION KokkosODE::Experimental::newton_solver_status NewtonSolve(
     norm = KokkosBlas::serial_nrm2(rhs);
 
     // Compute rms norm of the scaled update
+    norm_type norm_new = Kokkos::ArithTraits<norm_type>::zero();
     for (int idx = 0; idx < sys.neqs; ++idx) {
-      norm_new = (update(idx) * update(idx)) / (scale(idx) * scale(idx));
+      norm_new += (update(idx) * update(idx)) / (scale(idx) * scale(idx));
     }
     norm_new = Kokkos::sqrt(norm_new / sys.neqs);
     if ((it > 0) && norm_old > Kokkos::ArithTraits<norm_type>::zero()) {
